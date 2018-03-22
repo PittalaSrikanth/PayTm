a050c58a2a8fe4988d0c43a560c9224fc5b88c3b
srikanth <srikanth@srikanth-PC>
2018-03-22 23:41:52 +0530

    Main_1.03

--- realTime/src/FileReader/src/softwareTestingMaterial/ReadExcel.java
+++ realTime/src/FileReader/src/softwareTestingMaterial/ReadExcel.java
@@ -1,8 +1,7 @@
 package FileReader.src.softwareTestingMaterial;
- 
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
  
