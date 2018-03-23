bcc9a1dfdd79eead54a0041014bc2571ff923d86
wdev7 <wdev7@MHSI-W10-Wdev7.mhsindia.com>
2018-03-23 09:41:03 +0530

    Temp_main_1.01

---
+++ src/ExcelReader/ExcelTest.java
@@ -0,0 +1,16 @@
+package ExcelReader;
+
+import java.io.IOException;
+
+public class ExcelTest {
+	public static void main(String[] args) throws IOException {
+		ExcelXlsx.Connection("Book123.xlsx", "Sheet1");
+
+		ExcelXlsx.getData("W82");
+		ExcelXlsx.Sheet("Sheet2");
+		ExcelXlsx.getData("c5");
+
+		ExcelXlsx.closeExcel();
+
+	}
+}
---
+++ src/ExcelReader/ExcelXlsTest.java
@@ -0,0 +1,15 @@
+package ExcelReader;
+
+import java.io.IOException;
+
+import ExcelReader.ExcelXls;
+
+public class ExcelXlsTest {
+	public static void main(String[] args) throws IOException {
+		ExcelXls.Conncetion("Book123.xls", "Sheet1");
+
+		ExcelXls.getData("W82");
+		ExcelXls.closeExcel();
+
+	}
+}
