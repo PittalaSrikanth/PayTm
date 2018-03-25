94bcc53609003eb8ca64de5f469c36d4eaff04be
srikanth <srikanth@srikanth-PC>
2018-03-25 09:33:59 +0530

    Few_Updates03252018_01

---
+++ realTime/src/mhsi/actions/TestBase.java
@@ -0,0 +1,129 @@
+package mhsi.actions;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+public class TestBase {
+	public static WebDriver driver;
+
+	public static WebDriver selectBrowser(String browser) {
+		Reporter.log("Selected Browser is : " + browser);
+
+		if (browser.toLowerCase().equals("chrome")) {
+			System.setProperty(
+					"webdriver.chrome.driver",
+					System.getProperty("user.dir")
+							+ "\\src\\mhsi\\BrowserDrivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			Reporter.log(browser + ":Browser opend successfully");
+			driver.manage().window().maximize();
+			Reporter.log("Maximized " + browser + ":Browser");
+			return driver;
+		} else if (browser.toLowerCase().equals("firefox")) {
+			System.setProperty(
+					"webdriver.gecko.driver",
+					System.getProperty("user.dir")
+							+ "\\src\\mhsi\\BrowserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			Reporter.log(browser + ":Browser opend successfully");
+			driver.manage().window().maximize();
+			Reporter.log("Maximized " + browser + ":Browser");
+			return driver;
+		} else if (browser.toLowerCase().equals("ie")) {
+			System.setProperty("webdriver.ie.driver",
+					System.getProperty("user.dir")
+							+ "\\src\\mhsi\\BrowserDrivers\\ie.exe");
+			driver = new InternetExplorerDriver();
+			Reporter.log(browser + ":Browser opend successfully");
+			driver.manage().window().maximize();
+			Reporter.log("Maximized " + browser + ":Browser");
+			return driver;
+		}
+		return driver;
+
+	}
+
+	public static String getPageTittle() {
+		String result = driver.getTitle();
+		return result;
+
+	}
+
+	public static void implicitWait(int sec) {
+		Reporter.log("implicitWait for" + getPageTittle() + " page loading:"
+				+ sec);
+		try {
+			driver.manage().timeouts().implicitlyWait(sec, TimeUnit.SECONDS);
+			Reporter.log(getPageTittle() + "page loaded compleatly");
+		} catch (Throwable error) {
+			Reporter.log("Timeout for" + getPageTittle() + "page load in" + sec);
+		}
+	}
+
+	public static void explecitWati(WebElement element, int sec) {
+		try {
+			Reporter.log("explecitWati for" + getPageTittle()
+					+ " page loading:" + sec);
+			WebDriverWait wait = new WebDriverWait(driver, sec);
+			wait.until(ExpectedConditions.visibilityOf(element));
+			Reporter.log(getPageTittle() + "page loaded compleatly");
+		} catch (Throwable error) {
+			Reporter.log("Timeout for" + getPageTittle() + "page load in" + sec);
+		}
+	}
+
+	public static void mandatoryWait(int sec) throws InterruptedException {
+		try {
+			Reporter.log("mandatoryWait for" + getPageTittle()
+					+ " page loading:" + sec);
+			Thread.sleep(sec * 1000);
+			Reporter.log(getPageTittle() + "page loaded compleatly");
+		} catch (Throwable error) {
+			Reporter.log("Timeout for" + getPageTittle() + "page load in" + sec);
+		}
+	}
+
+	
+	public static String displayDateTime() {
+		Date d = new Date();
+		SimpleDateFormat s = new SimpleDateFormat("dd-MM-yyyy_HH_mm_ss");
+		return s.format(d);
+	}
+
+	public static void ExecutionStarts() {
+		Date d = new Date();
+		SimpleDateFormat s = new SimpleDateFormat("dd-MM-yyyy_HH_mm_ss");
+		Reporter.log("Execution Started at" + s.format(d));
+		// return s.format(d);
+	}
+
+	public static void ExecutionEnd() {
+		Date d = new Date();
+		SimpleDateFormat s = new SimpleDateFormat("dd-MM-yyyy_HH_mm_ss");
+		Reporter.log("Execution Ends at" + s.format(d));
+		// return s.format(d);
+	}
+
+	public static WebDriver navigate(String url) {
+		driver.navigate().to(url);
+		return driver;
+	}
+
+	public static void closeBrowser() {
+		driver.close();
+	}
+
+	public static void quitBrowser() {
+		driver.quit();
+	}
+}
--- realTime/src/mhsi/testScripts/login/TestLogin.java
+++ realTime/src/mhsi/testScripts/login/TestLogin.java
@@ -2,30 +2,48 @@ package mhsi.testScripts.login;
 
 import java.io.IOException;
 
-import mhsi.pageLibrary.LoginPage.LoginPage;
-import mhsi.testBase.TestBase;
+import mhsi.ExcelReader.ExcelXls;
+import mhsi.ExcelReader.ExcelXlsTest;
+import mhsi.ExcelReader.ExcelXlsx;
+import mhsi.customeListner.Listner1;
+import mhsi.testBase.Action;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
-public class TestLogin extends TestBase {
+public class TestLogin extends Listner1 {
 
 	@BeforeTest
 	public void setUp() throws IOException {
-		BrowserLanch();
+		Action.BrowserLanch();
+		Action.implicitWait(30);
 	}
 
 	@Test
 	public void applanch() throws Exception {
-		new LoginPage().loginApplication();
-		mandatoryWait(5);
+		ExcelXlsx.Connection("Login1.xlsx", "LoginPage_LoginTab");
 
+		Action.button(ExcelXlsx.getData("e2"), ExcelXlsx.getData("f2"));
+		Action.implicitWait(30);
+		Action.textbox(ExcelXlsx.getData("e3"), ExcelXlsx.getData("f3"),
+				ExcelXlsx.getData("g3"));
+		Action.button(ExcelXlsx.getData("e4"), ExcelXlsx.getData("f4"));
+		Action.implicitWait(30);
+		/*Action.link(ExcelXlsx.getData("e5"),ExcelXlsx.getData("f5"));
+		Action.textbox(ExcelXlsx.getData("e5"),ExcelXlsx.getData("f5"),
+				ExcelXlsx.ExcelXlsx.getData("g5"));*/
+		Action.button(ExcelXlsx.getData("e6"), ExcelXlsx.getData("f6"));
+		Action.button(ExcelXlsx.getData("e6"), ExcelXlsx.getData("f6"));
+		Action.button(ExcelXlsx.getData("e6"), ExcelXlsx.getData("f6"));
+		Action.button(ExcelXlsx.getData("e6"), ExcelXlsx.getData("f6"));
+		Action.implicitWait(30);
+		ExcelXlsx.closeExcel();
 	}
 
 	@AfterTest
 	public void clsApp() {
-		closeBrowser();
+		Action.closeBrowser();
 	}
 }
